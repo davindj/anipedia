f01138fa987e5b5c1be62b8119b58362fe8308a0
Davin DJ <davindx88@gmail.com>
2023-06-15 21:43:12 +0700

    feat: setup test-storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 const config: StorybookConfig = {
-  stories: ['../src/**/stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public', '../storybook-assets'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-coverage',
+      options: {
+        istanbul: {
+          include: ['**/src/views/**'],
+        },
+      },
+    },
     '@storybook/addon-a11y',
   ],
   framework: {
