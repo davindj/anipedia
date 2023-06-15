c84baab9db3488e9ef30a4463e2e6105f0e773e2
Davin DJ <davindx88@gmail.com>
2023-06-15 12:29:01 +0700

    feat: Collection List Page - full

--- src/views/pages/CollectionListPage/index.tsx
+++ src/views/pages/CollectionListPage/index.tsx
@@ -1,9 +1,61 @@
+import { useContext } from 'react'
 import MainLayout from '../../layouts/MainLayout'
+import AnimeCollectionContext from '../../../providers/AnimeCollectionsProvider/context'
+import {
+  CollectionItemsWrapper,
+  CollectionListPageWrapper,
+  TitlePage,
+} from './style'
+import { AnimeCollection } from '../../../entities/animeCollection'
+import { CollectionItem } from '../../components/CollectionItem'
+import {
+  Button,
+  ButtonColorEnum,
+  ButtonSizeEnum,
+  ButtonTypeEnum,
+} from '../../components/Button'
 
 const CollectionListPage = () => {
+  const {
+    animeCollections,
+    openAddCollectionModal,
+    openEditCollectionModal,
+    openRemoveCollectionModal,
+  } = useContext(AnimeCollectionContext)
+
+  // handler
+  const onAddButtonClicked = () => {
+    openAddCollectionModal()
+  }
+  const onEditCollectionItem = (collection: AnimeCollection) => () => {
+    openEditCollectionModal(collection)
+  }
+  const onRemoveCollectionItem = (collection: AnimeCollection) => () => {
+    openRemoveCollectionModal(collection)
+  }
+
   return (
     <MainLayout>
-      <h1>Collection List Page</h1>
+      <CollectionListPageWrapper>
+        <TitlePage>Anime Collections</TitlePage>
+        <Button
+          text={'Add a Collection'}
+          size={ButtonSizeEnum.NORMAL}
+          color={ButtonColorEnum.SUCCESS}
+          type={ButtonTypeEnum.FILL}
+          onClick={onAddButtonClicked}
+        />
+        <CollectionItemsWrapper>
+          {animeCollections.map(collection => (
+            <CollectionItem
+              key={collection.id}
+              collection={collection}
+              onEdit={onEditCollectionItem(collection)}
+              onRemove={onRemoveCollectionItem(collection)}
+            />
+          ))}
+        </CollectionItemsWrapper>
+      </CollectionListPageWrapper>
     </MainLayout>
   )
 }
---
+++ src/views/pages/CollectionListPage/style.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled'
+import { MEDIA_WIDTH_LG, MEDIA_WIDTH_MD } from '../../../constants/style'
+
+const CollectionListPageWrapper = styled.div`
+  padding: 20px;
+`
+
+const TitlePage = styled.h1`
+  font-weight: bold;
+  font-size: 2em;
+  margin: 0px;
+  margin-bottom: 20px;
+`
+
+const CollectionItemsWrapper = styled.div`
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 20px;
+  column-gap: 20px;
+
+  @media (min-width: ${MEDIA_WIDTH_MD}) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: ${MEDIA_WIDTH_LG}) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`
+
+export { CollectionListPageWrapper, TitlePage, CollectionItemsWrapper }
