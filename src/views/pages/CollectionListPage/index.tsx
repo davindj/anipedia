1c70c7ed6b0fdab17f8c98a4b5dd6e3752f9b612
Davin DJ <davindx88@gmail.com>
2023-06-15 13:37:05 +0700

    feat: CollectionListPage - title to my collections

--- src/views/pages/CollectionListPage/index.tsx
+++ src/views/pages/CollectionListPage/index.tsx
@@ -37,7 +37,7 @@ const CollectionListPage = () => {
   return (
     <MainLayout>
       <CollectionListPageWrapper>
-        <TitlePage>Anime Collections</TitlePage>
+        <TitlePage>My Collections</TitlePage>
         <Button
           text={'Add a Collection'}
           size={ButtonSizeEnum.NORMAL}
