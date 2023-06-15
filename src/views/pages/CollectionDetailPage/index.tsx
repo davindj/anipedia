52caa97f3322136488399bb85e3d194013b1921b
Davin DJ <davindx88@gmail.com>
2023-06-15 14:48:16 +0700

    feat: CollectionDetailPage - full

--- src/views/pages/CollectionDetailPage/index.tsx
+++ src/views/pages/CollectionDetailPage/index.tsx
@@ -1,12 +1,68 @@
-import MainLayout from '../../layouts/MainLayout'
-
+import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import MainLayout from '../../layouts/MainLayout'
+import AnimeCollectionContext from '../../../providers/AnimeCollectionsProvider/context'
+import { ButtonSection, CollectionDetailPageWrapper, TitlePage } from './style'
+import {
+  Button,
+  ButtonColorEnum,
+  ButtonSizeEnum,
+  ButtonTypeEnum,
+} from '../../components/Button'
+import { AnimeList } from '../../components/AnimeList'
+import ROUTE_PATH from '../../../routes/path'
 
 const CollectionDetailPage = () => {
+  // hooks
+  const {
+    isLoading,
+    animeCollections,
+    openEditCollectionModal,
+    openRemoveAnimeFromCollectionModal,
+  } = useContext(AnimeCollectionContext)
   const { id } = useParams()
+
+  // UI Fallback: Loading
+  if (isLoading) return <MainLayout>Loading...</MainLayout>
+
+  // Variables
+  const collection = animeCollections.find(collection => collection.id === id)
+
+  // UI Fallback: Collection not found
+  if (!collection) return <MainLayout>Collection not found</MainLayout>
+
+  // handler
+  const onEditCollectionName = () => {
+    openEditCollectionModal(collection)
+  }
+  const onRemoveItem = (idx: number) => {
+    const anime = collection.animes[idx]
+    openRemoveAnimeFromCollectionModal(anime, collection)
+  }
+
   return (
     <MainLayout>
-      <h1>Collection Detail Page for id: {id}</h1>
+      <CollectionDetailPageWrapper>
+        <NavLink to={ROUTE_PATH.COLLECTION_LIST_PAGE}>
+          Back to My Collections
+        </NavLink>
+        <TitlePage>{collection.name}</TitlePage>
+        <ButtonSection>
+          <Button
+            text={'Edit'}
+            size={ButtonSizeEnum.NORMAL}
+            color={ButtonColorEnum.SUCCESS}
+            type={ButtonTypeEnum.FILL}
+            onClick={onEditCollectionName}
+          />
+        </ButtonSection>
+        <AnimeList
+          isItemRemovable
+          animes={collection.animes}
+          onRemoveItem={onRemoveItem}
+        />
+      </CollectionDetailPageWrapper>
     </MainLayout>
   )
 }
---
+++ src/views/pages/CollectionDetailPage/style.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+const CollectionDetailPageWrapper = styled.div`
+  padding: 20px;
+`
+
+const TitlePage = styled.h1`
+  font-weight: bold;
+  font-size: 2em;
+  margin: 0px;
+  margin-bottom: 20px;
+`
+
+const ButtonSection = styled.div`
+  margin-bottom: 20px;
+`
+
+export { CollectionDetailPageWrapper, TitlePage, ButtonSection }
