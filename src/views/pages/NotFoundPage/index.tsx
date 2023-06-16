e7324d12e215c2d72cb844733bce8ad2ebe25c38
Davin DJ <davindx88@gmail.com>
2023-06-16 15:06:07 +0700

    feat: Not found page

--- src/views/pages/NotFoundPage/index.tsx
+++ src/views/pages/NotFoundPage/index.tsx
@@ -1,9 +1,39 @@
+import styled from '@emotion/styled'
 import MainLayout from '../../layouts/MainLayout'
+import { COLOR_SUCCESS_DARK, COLOR_TEXT_DARK } from '../../../utils/style'
+
+const NotFoundText = styled.p`
+  padding-top: 20vh;
+  font-size: 6em;
+  font-weight: bold;
+  text-align: center;
+  padding-inline: 20px;
+  margin: 0px;
+  color: ${COLOR_SUCCESS_DARK};
+`
+const NotFoundTitleText = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  padding-inline: 20px;
+  margin: 0px;
+  color: ${COLOR_SUCCESS_DARK};
+`
+const NotFoundDescriptionText = styled.p`
+  font-size: 1em;
+  text-align: center;
+  padding-inline: 20px;
+  margin: 0px;
+  color: ${COLOR_TEXT_DARK};
+`
 
 const NotFoundPage = () => {
   return (
     <MainLayout>
-      <h1>Not Found Page</h1>
+      <NotFoundText>404</NotFoundText>
+      <NotFoundTitleText>Page Not Found</NotFoundTitleText>
+      <NotFoundDescriptionText>
+        You got lost, but it's okay you can always go back to the right path.
+      </NotFoundDescriptionText>
     </MainLayout>
   )
 }
